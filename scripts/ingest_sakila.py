a71886d8e3678688f60a4b3e54dbf6cca1fcbc9b
daniela <danielaalgerydh@gmail.com>
2025-11-28 11:45:17 +0100

    Ingest sakila data into duckdb

--- scripts/ingest_sakila.py
+++ scripts/ingest_sakila.py
@@ -3,17 +3,21 @@ from dlt.sources.sql_database import sql_database
 from pathlib import Path
 
 # Constant paths
-DATA_PATH = Path(__file__).parent / 'data'
-SQLITE_PATH = DATA_PATH / 'sqlite-sakila.db'
-DUCKDB_PATH = DATA_PATH / 'sakila.duckdb'
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_PATH = BASE_DIR / "data"
+
+SQLITE_PATH = DATA_PATH / "sqlite-sakila.db"
+DUCKDB_PATH = DATA_PATH / "sakila.duckdb"
 
 #SQLite source (sakila db)
-source =sql_database(credentials=f"sqlite:///{SQLITE_PATH}", schema="main")
+source =sql_database(
+    credentials=f"sqlite:///{SQLITE_PATH}",
+    schema="main")
 
 # Define pipeline
 pipeline = dlt.pipeline(
     pipeline_name="sakila_loader",
-    destination=dlt.destinations.duckdb(duckdb_path=DUCKDB_PATH),
+    destination=dlt.destinations.duckdb(str(DUCKDB_PATH)),
     dataset_name="staging"
 )
 
