e5363ea398c4a5f32a030773d3f4213d2c730f8b
Daniela Algerydh <danielaalgerydh@gmail.com>
2025-12-08 11:43:29 +0100

    Got evidence finally up and running

--- evidence/sources/sakila/customer_payments.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    cu.customer_id,
-    cu.first_name || ' ' || cu.last_name AS customer,
-    p.amount,
-    p.payment_date
-FROM
-    staging.customer cu
-    JOIN staging.payment p ON p.customer_id = cu.customer_id;
--- evidence/sources/sakila/customers.sql
+++ evidence/sources/sakila/customers.sql
@@ -1,9 +1,12 @@
 SELECT
-    title,
-    rating,
-    length,
-    description
-FROM
-    staging.film
-WHERE
-    title ILIKE '%love%';
+    c.first_name || ' ' || c.last_name AS customer,
+    ct.city,
+    cr.country,
+    p.amount,
+    r.rental_id
+FROM staging.customer c
+LEFT JOIN staging.payment p on c.customer_id = p.customer_id
+LEFT JOIN staging.rental r ON c.customer_id = r.customer_id
+LEFT JOIN staging.address a ON a.address_id = c.address_id
+LEFT JOIN staging.city ct ON ct.city_id = a.city_id
+LEFT JOIN staging.country cr ON cr.country_id = ct.country_id;
