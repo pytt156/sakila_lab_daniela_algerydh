a738ac942b8fbee905667ece28a450ffd4329647
Daniela Algerydh <danielaalgerydh@gmail.com>
2025-12-08 15:26:21 +0100

    Add sql query for revenue

---
+++ evidence/sources/sakila/revenue.sql
@@ -0,0 +1,24 @@
+SELECT
+p.amount,
+c.first_name || ' ' || c.last_name as customer,
+sf.first_name || ' ' || sf.last_name as staff,
+f.title,
+ca.name as category,
+a.address,
+ci.city,
+co.country,
+st.store_id,
+r.rental_id
+FROM
+staging.rental r
+LEFT JOIN staging.payment p ON p.rental_id = r.rental_id
+LEFT JOIN staging.customer c ON c.customer_id = p.customer_id
+LEFT JOIN staging.staff sf ON p.staff_id = sf.staff_id
+LEFT JOIN staging.store st ON sf.store_id = st.store_id
+LEFT JOIN staging.inventory i ON r.inventory_id = i.inventory_id
+LEFT JOIN staging.film f ON f.film_id = i.film_id
+LEFT JOIN staging.film_category fc ON fc.film_id = f.film_id
+LEFT JOIN staging.category ca ON ca.category_id = fc.category_id
+LEFT JOIN staging.address a ON a.address_id = c.address_id
+LEFT JOIN staging.city ci ON ci.city_id = a.city_id
+LEFT JOIN staging.country co ON co.country_id = ci.country_id
